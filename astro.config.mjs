41b718041ba7e9929ec414b8ef78dc1ad614ff71
RoyLee <roy_lee@manka.co.jp>
2026-06-26 16:05:44 +0900

    Vulnerability fix: package upgrades

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,6 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   integrations: [react()],
   vite: {
-    plugins: [
-      // This most probably started to happen due to some Vite v6 / v7 discrepancies.
-      // Once it's patched properly, the type error should be gone.
-      // @ts-expect-error Type 'HotUpdatePluginContext' is missing the following properties from type 'MinimalPluginContext': debug, error, info, meta, warn
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
 });
